28b0d6e4bb8611dbe9ffa8f63800d03116069f25
Ashvini Gawande <ashvinigawande12@gmail.com>
2023-02-11 12:25:55 +0530

    Add my first project

---
+++ force-app/main/default/lwc/contactlist/contactlist.js
@@ -0,0 +1,13 @@
+import { api, LightningElement, wire } from 'lwc';
+import findContactByAccountId from '@salesforce/apex/ContactController.findContactByAccountId';
+
+export default class Contactlist extends LightningElement {
+    columns = [
+        { label: 'First Name', fieldName: 'FirstName'},
+        { label: 'Last Name', fieldName: 'LastName'},
+        { label: 'Email', fieldName: 'Email', type: 'email'},
+    ];
+    @api accountId;
+    @wire(findContactByAccountId,{accountId:'$accountId'}) contacts;
+
+}
---
+++ force-app/main/default/lwc/createOpportunity/createOpportunity.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class CreateOpportunity extends LightningElement {}
